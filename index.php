216f5c1298b282dfb06fa2d6987fbb882a8e7f88
Sergyung <sergbest@mail.ru>
2022-10-07 11:36:03 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	
+	echo'<p><a href="pageAddOffice.php">Добавить организацию</a></p>';
+	echo'<p><a href="">Добавить судно</a></p>';
+	echo'<p><a href="">Добавить двигатель</a></p>';
+	echo'<p><a href="">Добавить прибор</a></p>';
+	echo'<p><a href="">Проведение испытаний</a></p>';
+	echo'<a href="protocol.php" target="_blank">Протокол</a>';
+
+?>
